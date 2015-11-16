2ffeb2b4727215131a8f80762529cd3f01cd652d
Monica <mbarreracastillo@gmail.com>
2015-11-15 20:59:55 -0500

    Programa que grafica

---
+++ poblaciones/poblaciones.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data=np.loadtxt("poblaciones.dat")
+a=data[:,0]
+b=data[:,1]
+c=data[:,2]
+d=data[:,3]
+l=data[:,4]
+
+plt.figure()
+f,fig = plt.subplots(3,3,figsize=(12,10))
+
+fig[0,0].scatter(a,b)
+fig[0,0].set_ylabel('b')
+fig[0,0].set_xlabel('a')
+
+fig[1,0].scatter(a,c)
+fig[1,0].set_ylabel('c')
+fig[1,0].set_xlabel('a')
+
+fig[2,0].scatter(a,d)
+fig[2,0].set_ylabel('d')
+fig[2,0].set_xlabel('a')
+
+fig[0,1].axis('off')
+
+fig[1,1].scatter(b,c)
+fig[1,1].set_ylabel('c')
+fig[1,1].set_xlabel('b')
+
+fig[2,1].scatter(b,d)
+fig[2,1].set_ylabel('d')
+fig[2,1].set_xlabel('b')
+
+fig[0,2].axis('off')
+
+fig[1,2].axis('off')
+
+fig[2,2].scatter(c,d)
+fig[2,2].set_ylabel('d')
+fig[2,2].set_xlabel('c')
+
+plt.savefig("Probabilidades_poblaciones.pdf",format="pdf")
+plt.close()
